28ec709ee03c43736c84aca2d7abfa587740e23f
Eduardo-Vinicius-dos-SR <eduardovdsr@gmail.com>
2026-01-19 17:02:10 -0300

    fix: não retorna o password no registro do usuário

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,7 +3,6 @@ import { prisma } from "../utils/prisma";
 import bcrypt from "bcrypt";
 
 export const registerUser = async (payload: RegisterRequest) => {
-
 	const existingUser = await prisma.user.findUnique({
 		where: { email: payload.email },
 	});
@@ -12,7 +11,7 @@ export const registerUser = async (payload: RegisterRequest) => {
 		throw new Error("Email já cadastrado.");
 	}
 
-    const hashedPassword = await bcrypt.hash(payload.password, 10);
+	const hashedPassword = await bcrypt.hash(payload.password, 10);
 
 	const newUser = await prisma.user.create({
 		data: {
@@ -23,27 +22,41 @@ export const registerUser = async (payload: RegisterRequest) => {
 			cpf: payload.cpf,
 			birthDate: payload.dateOfBirth || undefined,
 			phone: payload.phone,
-            role: "USER",
+			role: "USER",
+		},
+		select: {
+			id: true,
+			firstName: true,
+			lastName: true,
+			email: true,
+			cpf: true,
+			birthDate: true,
+			phone: true,
+			role: true,
+			createdAt: true,
 		},
 	});
 
-    return newUser;
+	return newUser;
 };
 
 export const loginUser = async (data: AuthRequest) => {
-    const user = await prisma.user.findUnique({
-        where: { email: data.email },
-    });
+	const user = await prisma.user.findUnique({
+		where: { email: data.email },
+	});
 
-    if(!user) {
-        throw new Error("Usuário não encontrado.");
-    }
+	if (!user) {
+		throw new Error("Usuário não encontrado.");
+	}
 
-    const isValidPassword = await bcrypt.compare(data.password, user.password);
+	const isValidPassword = await bcrypt.compare(data.password, user.password);
 
-    if(!isValidPassword) {
-        throw new Error("Senha inválida.");
-    }
+	if (!isValidPassword) {
+		throw new Error("Senha inválida.");
+	}
 
-    return user;
+	// Remover password antes de retornar
+	const { password, ...userWithoutPassword } = user;
+
+	return userWithoutPassword;
 };
